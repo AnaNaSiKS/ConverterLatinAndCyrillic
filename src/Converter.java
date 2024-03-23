59241b74fcd9b081265ba2d69cbce020cf2a3c85
Влад Кантамиров <kantamirov2016@gmail.com>
2024-03-23 21:44:26 +0300

    init

---
+++ src/Converter.java
@@ -0,0 +1,11 @@
+public class Converter implements IConverter{
+    @Override
+    public String cyrillicToLatin(String value) {
+        return null;
+    }
+
+    @Override
+    public String latinToCyrillic(String value) {
+        return null;
+    }
+}
---
+++ src/IConverter.java
@@ -0,0 +1,4 @@
+public interface IConverter {
+    public String cyrillicToLatin(String value);
+    public String latinToCyrillic(String value);
+}
